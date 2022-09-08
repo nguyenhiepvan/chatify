fb6e8cde09f6d503dc38585bbd5b54433a7f1775
Nguyễn Hiệp <33170716+nguyenhiepvan@users.noreply.github.com>
2022-09-08 10:12:53 +0700

    Update footerLinks.blade.php

--- src/views/layouts/footerLinks.blade.php
+++ src/views/layouts/footerLinks.blade.php
@@ -1,18 +1,22 @@
-<script src="https://js.pusher.com/7.0.3/pusher.min.js"></script>
-<script >
-  // Enable pusher logging - don't include this in production
-  Pusher.logToConsole = true;
+<script src="https://js.pusher.com/7.4.0/pusher.min.js"></script>
+<script>
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
 
-  var pusher = new Pusher("{{ config('chatify.pusher.key') }}", {
-    encrypted: true,
-    cluster: "{{ config('chatify.pusher.options.cluster') }}",
-    authEndpoint: '{{route("pusher.auth")}}',
-    auth: {
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    const pusher = new Pusher("{{ config('chatify.pusher.key') }}", {
+        wsHost: '{{ config('chatify.pusher.options.host') }}',
+        wsPort: '{{ config('chatify.pusher.options.port') }}',
+        forceTLS: false,
+        encrypted: true,
+        // disableStats: true,
+        enabledTransports: ['ws', 'wss'],
+        authEndpoint: '{{route("pusher.auth")}}',
+        auth: {
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
         }
-    }
-  });
+    });
 
     // Bellow are all the methods/variables that using php to assign globally.
     const allowedImages = {!! json_encode(config('chatify.attachments.allowed_images')) !!} || [];
